c59953dce765c7486f39b0707c8ad3b6fbd5f0b9
Oski <github email address>
2018-10-07 07:50:12 -0400

    Added comments for the 7 restful routes required

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,16 +24,32 @@ namespace CucaFunApi.Controllers
             }
         }
 
+        //Restful Routes
+        //GET index (/users/)
         [HttpGet]
         public ActionResult<List<User>> GetAll()
         {
             return _context.Users.ToList();
         }
 
+        //POST create (/users/)
         [HttpPost(Name="NewUser")]
         public void UserUpdate([FromBody] string value)
         {
 
         }
+
+        //GET new (/users/new)
+
+        //GET edit (/users/{:id}/edit)
+
+        //GET show (/users/{:id})
+
+        //PATCH update (/users/{:id}/update) | Modifies existing data
+
+        //PUT update (/users/{:id}/update) | Replaces existing data
+
+        //DELETE destroy (/users/{:id})
+
     }
 }
